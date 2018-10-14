dc5e717604a527aee399ce61bc7a3d92d708befc
XiangyiCheng <xxc283@case.edu>
2018-10-14 16:29:36 -0400

    test

---
+++ Desktop.py
@@ -0,0 +1,21 @@
+import cv2
+import socket
+import threading
+import numpy
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+s.bind(("192.168.1.124", 9901))
+
+def reveice_play(s,sc):
+	data,addr = s.recvfrom(64000)
+	data = numpy.fromstring(data, dtype = 'uint8')
+	image = cv2.imdecode(data, 1)
+	image = cv2.resize(image, (640,480))
+	cv2.imshow(sc, image)
+	cv2.waitKey(10)
+
+while True:
+    reveice_play(s, "play")
+
+    
---
+++ Robot.py
@@ -0,0 +1,36 @@
+import picamera
+import picamera.array
+from time import sleep
+import cv2
+
+import socket
+
+camera = picamera.PiCamera()
+
+#camera.resolution = (1024,768)
+
+#rawCapture = picamera.array.PiRGBArray(camera,size=(1024,768))
+
+camera.resolution = (320,240)
+rawCapture = picamera.array.PiRGBArray(camera,size=(320,240))
+
+s_v = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+
+sleep(0.1)
+
+for frame in camera.capture_continuous(rawCapture,format='bgr',use_video_port=True):
+    image = frame.array
+
+    result, imgencode = cv2.imencode('.jpg',image) 
+    
+    cv2.imshow('frame',image)
+
+    s_v.sendto(imgencode, ("192.168.1.124", 9901))
+
+    
+    
+    rawCapture.truncate(0)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
