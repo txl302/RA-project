c47e40ecb463829511852ab5dc6c0b38a60b7183
txl302 <txl302@case.edu>
2019-03-04 18:57:42 -0800

    webs

---
+++ cloud.py
@@ -0,0 +1,22 @@
+import cv2
+import socket
+
+import numpy
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+s.bind(("35.185.4.47", 9901))
+
+def reveice_save(s,sc):
+	data,addr = s.recvfrom(64000)
+	data = numpy.fromstring(data, dtype = 'uint8')
+	image = cv2.imdecode(data, 1)
+	image = cv2.resize(image, (640,480))
+	cv2.imshow(sc, image)
+	cv2.imwrite('image_s.jpg', image)
+	cv2.waitKey(10)
+
+while True:
+    reveice_save(s, "play")
+
+    
