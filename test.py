ba303a64d64c75a9370ae585588cba0da1524aa4
txl302 <txl302@case.edu>
2019-03-06 19:25:58 -0800

    face

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import socket
+from time import sleep
 
 s_v = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
 
-    s_v.sendto("haha", ("192.168.1.124", 9901))
-
-    sleep(1)
+	s_v.sendto("haha", ("35.185.47.4", 9901))
+	s_v.sendto("haha", ("192.168.1.124", 9901))
